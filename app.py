3998fd4e020efc9f55d202ca598dd8d55b5589a6
sara latorre <saralatorre97@gmail.com>
2025-07-12 10:50:45 +0200

    added study analysis to the app

--- app.py
+++ app.py
@@ -61,7 +61,13 @@ width = 0.25 # width of each bar
 
 # 1. Sleep disorder distribution by occupation bar chart
 st.subheader("Question 1: Which occupations report the highest percentage of sleep disorders?")
-st.write("Hello! This is a general-purpose way to write text.")
+
+st.markdown("""
+This section analyzes which professions report the **highest prevalence of sleep disorders**.
+
+The chart below displays the **percentage distribution** of each sleep disorder (or lack thereof) across occupations.  
+This helps highlight whether certain jobs are more strongly associated with sleep-related health issues.
+""")
 
 # count the number of occurrences for each occupation/ sleep disorder pair
 disorder_counts = filtered_df.groupby(["Occupation", "Sleep Disorder"]).size().unstack(fill_value=0)
@@ -101,14 +107,33 @@ for i in range(len(labels)):
 # display the chart
 st.pyplot(fig)
 
-st.write("Sales workers are the most affected by sleep disorders, with 85.3% reporting insomnia, 8.8% reporting sleep apnea, and only 5.9% reporting no disorder.")
-st.write("Nurses report the highest levels of sleep apnea (83.6%) which is significantly higher than in any other profession surveyed where sleep apnea rates range from 0% to just 10%.")
-st.write("Teachers also show high insomnia levels (67.5%) and only 22.5% with no disorder.")
-st.write("In contrast, Doctors, Engineers, Accountans, and Lawyers have the lowest disorder rates. Over 80% of individuals in each of those professions report no sleep disorder.")
+st.markdown("#### Conclusion")
+
+st.markdown("""
+→ **Sales workers** are the **most affected by sleep disorders**:  
+- **85.3%** report **insomnia**  
+- **8.8%** report **sleep apnea**  
+- Only **5.9%** report **no disorder**
+            
+
+→ **Nurses** have the **highest rate of sleep apnea** by far (**83.6%**), significantly exceeding all other professions (ranging from 0% to ~10%).
+
+→ **Teachers** also show **high insomnia prevalence** (**67.5%**), and only **22.5%** report **no disorder**.
+
+→ In contrast, **Doctors, Engineers, Accountants, and Lawyers** show the **lowest disorder rates**, with **over 80%** in each group reporting **no sleep disorder**.
+""")
+
 
 st.markdown("---")
 # 2. Sleep duration by stress level bar chart with stress, sleep quality, physical activity)
 st.subheader("Question 2: Do occupations with higher stress levels also report lower sleep quality or fewer sleep hours?")
+
+st.markdown("""
+This analysis examines whether occupations with **higher average stress levels** also tend to report **lower sleep quality** or **shorter sleep duration**.
+
+The chart below compares **stress levels**, **sleep duration**, and **sleep quality** across professions.  
+While the focus is on stress and sleep, **physical activity** is included as a secondary factor to observe whether it may also play a role in sleep health or if **stress remains the stronger predictor**.
+""")
 
 # calculate means by Occupation
 stress_levels = filtered_df.groupby("Occupation")["Stress Level"].mean().reindex(labels)
@@ -164,17 +189,41 @@ ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper left')
 # display chart
 st.pyplot(fig)
 
-st.write("There’s a clear correlation between higher stress and poorer sleep health.")
-st.write("Sales workers report the highest stress level (7.1), lowest sleep duration (6.4 hours), and worst sleep quality (5.9).")
-st.write("Doctors also show high stress (6.7) and poor sleep quality (6.6), although their sleep duration is slightly better (7 hours).")
-st.write("Engineers, on the other hand, report the lowest stress (4.0), highest sleep duration (7.9 hours), and best sleep quality (8.3).")
-st.write("Interestingly, physical activity levels appear to vary independently as nurses (78.6) and lawyers (70.4) report the highest activity, while engineers (51.6) and sales workers (44.1) are less active. Despite lower activity, engineers report excellent sleep, which may mean stress has a stronger influence on sleep health than physical activity alone.")
+st.markdown("#### Conclusion")
+
+st.markdown("""
+
+There’s a clear correlation between **higher stress** and **poorer sleep health**.
+
+→ **Sales workers** report the **highest stress level** (**7.1**), **lowest sleep duration** (**6.4 hours**), and **worst sleep quality** (**5.9**).
+
+→ **Doctors** also show **high stress** (**6.7**) and **poor sleep quality** (**6.6**), although their sleep duration is slightly better (**7 hours**).            
+
+→ **Engineers**, on the other hand, report the **lowest stress** (**4.0**), **highest sleep duration** (**7.9 hours**), and **best sleep quality** (**8.3**).
+            
+**Physical Activity Comparison**  
+- **Most Active:** Nurses (78.6), Lawyers (70.4)  
+- **Least Active:** Engineers (51.6), Sales Workers (44.1)
+            
+→ Interestingly, **physical activity levels** appear to vary independently as **nurses (78.6)** and **lawyers (70.4)** report the highest activity, while **engineers (51.6)** and **sales workers (44.1)** are less active. Despite lower activity, engineers report excellent sleep, which may mean **stress has a stronger influence on sleep health** than physical activity alone.
+""")
+
+
 
 st.markdown("---")
 # 3. BMI Category distribution by occupation bar chart with secondary axis for average age
 st.subheader("Question 3: Are there noticeable differences in BMI category distribution across occupations?")
 
-st.write("This analysis shows whether specific jobs are linked with higher rates of overweight or obesity.")
+st.markdown("""
+This analysis investigates how rates of **overweight** and **obesity** vary across different occupations.  
+Since both **age** and **gender** are known to influence body mass index, we include them as important factors to contextualize the BMI differences observed.
+
+- **Age** affects metabolism and fat distribution, often increasing BMI as people get older ([Jura M, Kozak LP., 2016](https://pmc.ncbi.nlm.nih.gov/articles/PMC5005878/)).  
+- **Gender** influences body composition and fat patterns, with different prevalence of overweight/obesity between genders ([Koceva A, Herman R, Janez A, Rakusa M, Jensterle M., 2024](https://pmc.ncbi.nlm.nih.gov/articles/PMC11242171/)).
+
+By examining BMI together with average age and gender distribution, we can better understand the patterns by occupation in weight variations and identify whether age or gender alone can explain these differences.
+""")
+
 
 # filter for BMI categories
 bmi_options = ["Overweight", "Obese"]
@@ -232,31 +281,37 @@ ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper right')
 # Show chart
 st.pyplot(fig)
 
-st.write("There are noticeable and significant differences in BMI category distribution across occupations, such as:")
-st.write("Sales workers, nurses, and teachers are much more likely to be overweight or obese (85%–100% of participants)")
-st.write("Doctors, engineers, and lawyers maintain predominantly healthy weights")
-st.write("Is age or physical activity enough to explain this?")
-st.write("No, because while physical activity is generally associated with better weight management, and age can influence metabolism, they don't fully explain the differences because:")
-st.write("Nurses have the highest physical activity level, yet also have poor BMI results.")
-st.write("Similarly, engineers are older on average and have only moderate physical activity (51.6), yet maintain healthy BMI levels.")
-st.write("This suggested that other factorssuch as sleep quality & hours, stress, or working conditions might play a more important role in shaping BMI results.")
-st.write("What about gender?")
-st.write("Occupations with the highest overweight/obesity rates tend to be female-dominated (e.g. nurses, teachers).")
-st.write("However, sales, a male-dominated field, also has extremely poor BMI results, which suggest that gender is also not enough to explain that alone the differences in BMI across occupations.")
+st.markdown("#### Conclusion")
+
+st.markdown("""
+→ **Sales workers, nurses, and teachers** exhibit the **highest rates of overweight and obesity** (ranging between **85% and 100%** of participants).
+
+→ **Doctors, engineers, and lawyers** tend to maintain **predominantly healthy BMI levels**.
+
+→ **Age and physical activity alone do not fully explain BMI differences**:  
+- Nurses have the **highest physical activity levels** but still poor BMI outcomes.  
+- Engineers are on average **older** and moderately active (**51.6**), yet maintain healthy weights.
+
+→ This suggests **other factors** such as **sleep quality, stress, or working conditions** may significantly influence BMI.
+
+→ Regarding **gender distribution**:  
+- Occupations with higher overweight/obesity rates are often **female-dominated** (e.g., nurses, teachers).  
+- However, sales (a **male-dominated field**) also reports very poor BMI results, indicating gender alone does **not fully explain** the differences.
+
+""")
 
 
 st.markdown("---")
 # 4. Correlation: BMI vs Sleep Quality and Sleep Duration
 st.subheader("Extended Conclusion: Revisiting BMI with Sleep as a Key Factor")
 
-st.markdown("**Additional question:** *What if sleep quality and sleep duration were stronger predictors of BMI than gender or age?*")
+st.markdown("""**Additional question:** *What if sleep quality and sleep duration were stronger predictors of BMI than gender or age?*
 
-st.markdown("""
 While the initial analysis of question n°3 showed clear differences in BMI across occupations,  
 factors like **age**, **gender**, and **physical activity** offered only **partial explanations**.
+            
+This section explores the relationship between BMI rates and sleep metrics (quality and duration) across occupations.            
 """)
-
-
 
 
 # Combine BMI categories
@@ -298,28 +353,55 @@ st.pyplot(fig)
 st.markdown("#### Conclusion")
 
 st.markdown("""
-By comparing the percentage of overweight and obese individuals in each occupation with their average sleep quality and duration, I discovered the following patterns:
+By comparing the percentage of overweight and obese individuals in each occupation with their average sleep quality and duration, we can observe the following patterns:
 
-#### Sales Workers:
-- **Worst sleep quality**: 5.9  
-- **Shortest sleep duration**: 6.4 hours  
-- **Highest BMI issues**: ~100% overweight/obese
+→ **Sales Workers** suffer from the **worst sleep quality** (5.9) and **shortest sleep duration** (6.4 hours),  
+resulting in the **highest overweight/obesity rates** (~100%).
 
-#### Teachers & Nurses:
-- **Poor sleep health**: 6.3–6.4 quality, 6.6 hours  
-- **Very high BMI issues**: ~85%–90%
+→ **Teachers and Nurses** experience **poor sleep health** (quality between 6.3–6.4 and about 6.6 hours sleep),  
+with **very high BMI issues** (~85%–90%).
 
-#### Engineers & Lawyers
-- **Best sleep health**: 7.8–8.3 quality, 7.6–7.9 hours  
-- **Healthiest BMI profiles**
+→ **Engineers and Lawyers** enjoy the **best sleep health** (quality between 7.8–8.3 and 7.6–7.9 hours sleep),  
+and have the **healthiest BMI profiles**.
 
-#### Doctors are the exception:
-- **Poorer sleep than expected**: 6.6 quality, 7.0 hours  
-- **Better BMI than engineers**, suggesting external **protective factors** like:
-  - Medical knowledge  
-  - Healthier habits  
-  - Better dietary choices
+→ **Doctors are an exception**: despite **poorer sleep quality than engineers** (6.6 quality, 7.0 hours),  
+they maintain better BMI results, likely due to **protective factors** such as:  
+- Medical knowledge  
+- Healthier lifestyle habits  
+- Better dietary choices
 
-#### What it demonstrates:          
-Sleep metrics (quality and hours) appear to better align with BMI patterns than other factors. Occupations with poor sleep = poor BMI results, and those with better sleep = healthier BMI result. Additionally, the results also suggest that possible protective factors can also play a strong role in BMI results.
+→ ** What it demonstrates:** 
+Sleep metrics (quality and hours) appear to better align with BMI patterns than other factors.
+Occupations with poor sleep tend to have worse BMI outcomes. Additionally, the results also suggest that possible protective factors can also play a strong role in BMI results.
+""")
+
+st.markdown("---")
+
+st.subheader("Study Conclusion")
+
+st.markdown("""
+This study examined the relationships between occupation, sleep disorders, BMI categories, and sleep quality/duration to understand health disparities **across professions**.
+
+##### Key insights:
+
+- Certain occupations, such as **sales workers, nurses, and teachers**, show significantly higher rates of sleep disorders (especially insomnia and sleep apnea) and elevated BMI levels, which highlights a concerning overlap between poor sleep health and weight issues.
+
+- In contrast, **doctors, engineers, accountants, and lawyers** tend to have lower prevalence of sleep disorders and healthier BMI levels. This may reflect not only greater health awareness, but also more stable work environments, structured routines, and access to resources that support healthier lifestyles. 
+
+- For example, while doctors report poorer sleep than expected, they still maintain relatively healthy BMI levels, likely due to the nature of their profession, which involves medical training and knowledge, and a culture that emphasizes preventive care and healthy habits.
+
+- Similarly, engineers and lawyers, despite mostly sedentary work, show better sleep and BMI profiles, suggesting that job structure plays a key role in shaping a healthy lifestyle.
+            
+While factors like age, gender, and physical activity do play a role, they do not fully account for the differences observed across occupations. This suggests that other elements, such as sleep or stress may have a stronger and more direct influence on health outcomes.
+
+Therefore, **sleep quality and duration emerge as strong predictors of BMI status across occupations**, as those with poorer sleep consistently show higher rates of overweight and obesity. These findings demonstrate the importance of considering workplace conditions when addressing public health issues related to weight and well-being.
+
+**Protective factors** such as medical knowledge and healthier habits appear to buffer some groups (e.g., doctors) against poor BMI outcomes despite less ideal sleep metrics.
+
+##### Implications:
+            
+Understanding how work influences and is interconnected to sleep and BMI results can help shape more effective workplace health initiatives. Occupation factors can affect sleep habits, stress levels and long-term weight outcomes. These results emphasize the need for employers to address work-related factors and that targeting occupation-specific challenges could lead to more successful measures. 
+            
+Future research should explore how specific occupational characteristics interact with sleep and lifestyle behaviors to influence overall health outcomes, as well as how improvements in sleep quality and general health can enhance workplace productivity and performance.
+
 """)
